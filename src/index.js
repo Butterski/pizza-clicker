f7597983a6cacc12b5725ea214094fd193047061
milosz.kucharski <miloszkucharski@gmail.com>
2022-11-24 18:16:12 +0100

    removed tests

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 import { PersistGate } from "redux-persist/integration/react";
 import {persistor} from './redux/store';
@@ -12,16 +11,10 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
     <PersistGate loading={null} persistor={persistor}>
       <Provider store={store}>
         <App />
       </Provider>
     </PersistGate>
-  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
